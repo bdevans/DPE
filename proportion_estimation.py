e958e71442a316073eefee7c4b42bb9873c4d8cc
Ben Evans <b.d.evans@exeter.ac.uk>
2018-11-10 22:52:02 +0000

    Split line

--- proportion_estimation.py
+++ proportion_estimation.py
@@ -48,7 +48,8 @@ def fit_kernels(scores, bw):
 # @mem.cache
 def fit_KDE_model(Mix, bins, model, params_mix, kernel):
     x_KDE = bins["centers"]
-    kde_mix = KernelDensity(kernel=kernel, bandwidth=bins['width']).fit(Mix[:, np.newaxis])
+    kde_mix = KernelDensity(kernel=kernel, bandwidth=bins['width'])
+    kde_mix.fit(Mix[:, np.newaxis])
     res_mix = model.fit(np.exp(kde_mix.score_samples(x_KDE[:, np.newaxis])),
                         x=x_KDE, params=params_mix)
     amp_Ref1 = res_mix.params['amp_Ref1'].value
