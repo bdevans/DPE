6b51d7d02668e61ee2792688575d37cbf15177d0
Ben Evans <b.d.evans@exeter.ac.uk>
2020-11-01 18:03:58 +0000

    Simplify floor division

--- figures.py
+++ figures.py
@@ -1063,7 +1063,7 @@ if __name__ == "__main__":
             # Split the references distributions to ensure i.i.d. data for 
             # constructing the mixtures and estimating them.
             n_RefC, n_RefN = len(scores['Ref1']), len(scores['Ref2'])
-            partition_RefC, partition_RefN = int(n_RefC/2), int(n_RefN/2)
+            partition_RefC, partition_RefN = n_RefC//2, n_RefN//2
             inds_RefC = np.random.permutation(n_RefC)
             inds_RefN = np.random.permutation(n_RefN)
             hold_out_scores = {'Ref1': scores['Ref1'][inds_RefC[:partition_RefC]],
