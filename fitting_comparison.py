d61fcd719519f198712a6d3fcace374838466f44
Ben Evans <b.d.evans@exeter.ac.uk>
2020-12-22 17:46:38 +0000

    Overhaul notation, move Timer and test bounds with curve_fit

--- fitting_comparison.py
+++ fitting_comparison.py
@@ -5,58 +5,50 @@ Created on Thu Jul  5 12:35:23 2018
 
 @author: ben
 """
-import time
+# Script to benchmark performance of the KDE method
+# There are two main computational costs
+# 1. Convolving kernels with the data. 
+# This depends upon: the kernel, bandwidth, number of points and the algorithm
+# 2. The least-squares fitting algorithm
+
 from pprint import pprint
 
+import numpy as np
 from scipy.optimize import curve_fit
-
-from lmfit import minimize
-
+# from lmfit import minimize
+import lmfit
 import corner
+from sklearn.neighbors import KernelDensity
+from matplotlib import pyplot as plt
 
+from datasets import load_diabetes_data, load_coeliac_data
+from proportion_estimation import analyse_mixture
+from proportion_estimation import fit_kernel
+from utilities import Timer
 
-class Timer:
-    """
-    Class for timing blocks of code.
-    http://preshing.com/20110924/timing-your-code-using-pythons-with-statement/
-    Examples
-    --------
-    >>> with Timer() as t:
-    >>>    run code...
-    Execution took <t>s)
-    """
-    # interval = 0
-    def __init__(self):
-        self.start = 0
-        self.end = 0
-        self.interval = 0
+# scores, bins, means, medians, p_C = load_diabetes_data('T1GRS')
+scores, bins, means, medians, p_C = load_coeliac_data()
 
-    def __enter__(self):
-        self.start = time.clock()
-        return self
-
-    def __exit__(self, *args):
-        self.end = time.clock()
-        self.interval = self.end - self.start
-        print('{:.3g}s'.format(self.interval))
-
-    def __str__(self):
-        return '{:.3g}s'.format(self.interval)
-
-    def reset(self):
-        """Reset timer to 0."""
-        self.start = 0
-        self.end = 0
-        self.interval = 0
+# seed = 42
+# n_boot = 10
+# sample_size = 1000  # -1
+# n_mix = 10
+# alpha = 0.05
+# methods = 'all'
+# n_jobs = 1
+# ci_method = "bca"
+# correct_bias = False
+# kernel = "gaussian"
+# n_head = 50
 
 # ------------------------------ KDE method ------------------------------
 
 plot_results = True
-verbose = 1
+verbose = 0
 
 KDE_kernel = "gaussian"
 bw = bins["width"]
-print("Using {} kernel with bandwith = {}".format(KDE_kernel, bw))
+print(f"Using {KDE_kernel} kernel with bandwith = {bw}")
 
 # TODO: https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.leastsq.html
 
@@ -79,18 +71,18 @@ x = bins["centers"]
 y = np.exp(kdes["Mix"][KDE_kernel].score_samples(x[:, np.newaxis]))
 
 
-# SciPy
+# SciPy. This defaults to method 'lm'
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html
 print("="*40)
 print("SciPy curve_fit")
 print("="*40)
 
-def KDE_Mix(x, amp_Ref1, amp_Ref2):
-    '''The model function, f(x, …). It must take the independent variable as the
+def KDE_Mix(x, amp_R_C, amp_R_N):
+    '''The model function, f(x, ...). It must take the independent variable as the
     first argument and the parameters to fit as separate remaining arguments.'''
-    dens_Ref1 = np.exp(kdes['Ref1'][KDE_kernel].score_samples(x[:, np.newaxis]))
-    dens_Ref2 = np.exp(kdes['Ref2'][KDE_kernel].score_samples(x[:, np.newaxis]))
-    return amp_Ref1 * dens_Ref1 + amp_Ref2 * dens_Ref2
+    dens_R_C = np.exp(kdes['R_C'][KDE_kernel].score_samples(x[:, np.newaxis]))
+    dens_R_N = np.exp(kdes['R_N'][KDE_kernel].score_samples(x[:, np.newaxis]))
+    return amp_R_C * dens_R_C + amp_R_N * dens_R_N
 
 p0 = (1, 1)
 
@@ -99,10 +91,29 @@ with Timer() as t:
 
 print("Parameters:", popt)
 print("Covariance:", pcov)
-amp_Ref1, amp_Ref2 = popt
-print("Estimated proportion:", amp_Ref1/(amp_Ref1+amp_Ref2))
+amp_R_C, amp_R_N = popt
+print("Estimated proportion:", amp_R_C/(amp_R_C+amp_R_N))
 print()
 
+
+# SciPy with bounds. This uses method 'trf'.
+# Internally, this calls least_squares instead of leastsq
+print("="*40)
+print("SciPy curve_fit (with bounds)")
+print("="*40)
+
+p0 = (1, 1)
+# bounds = (0, 1)  # 2-tuple of arrays (separate bounds for each param) or scalars
+bounds = (0, np.inf)
+
+with Timer() as t:
+    popt, pcov = curve_fit(KDE_Mix, x, y, p0, bounds=bounds)
+
+print("Parameters:", popt)
+print("Covariance:", pcov)
+amp_R_C, amp_R_N = popt
+print("Estimated proportion:", amp_R_C/(amp_R_C+amp_R_N))
+print()
 
 # lmfit model
 print("="*40)
@@ -113,58 +124,55 @@ print("="*40)
 #    x_KDE = np.linspace(bins['min'], bins['max'], len(Mix)+2)
 #    mix_kde = KernelDensity(kernel=kernel, bandwidth=bins['width']).fit(Mix[:, np.newaxis])
 #    res_mix = model.fit(np.exp(mix_kde.score_samples(x_KDE[:, np.newaxis])), x=x_KDE, params=params_mix)
-#    amp_Ref1 = res_mix.params['amp_Ref1'].value
-#    amp_Ref2 = res_mix.params['amp_Ref2'].value
-#    return amp_Ref1/(amp_Ref1+amp_Ref2)
+#    amp_R_C = res_mix.params['amp_R_C'].value
+#    amp_R_N = res_mix.params['amp_R_N'].value
+#    return amp_R_C/(amp_R_C+amp_R_N)
 
 
 # Define the KDE models
 # x := Bin centres originally with n_bins = int(np.floor(np.sqrt(N)))
-def kde_Ref1(x, amp_Ref1):
-    return amp_Ref1 * np.exp(kdes['Ref1'][KDE_kernel].score_samples(x[:, np.newaxis]))
+def kde_R_C(x, amp_R_C):
+    return amp_R_C * np.exp(kdes['R_C'][KDE_kernel].score_samples(x[:, np.newaxis]))
 
-def kde_Ref2(x, amp_Ref2):
-    return amp_Ref2 * np.exp(kdes['Ref2'][KDE_kernel].score_samples(x[:, np.newaxis]))
+def kde_R_N(x, amp_R_N):
+    return amp_R_N * np.exp(kdes['R_N'][KDE_kernel].score_samples(x[:, np.newaxis]))
 
-model = lmfit.Model(kde_Ref1) + lmfit.Model(kde_Ref2)
+model = lmfit.Model(kde_R_C) + lmfit.Model(kde_R_N)
 
 params_mix = model.make_params()
-params_mix['amp_Ref1'].value = 1
-params_mix['amp_Ref1'].min = 0
-params_mix['amp_Ref2'].value = 1
-params_mix['amp_Ref2'].min = 0
+params_mix['amp_R_C'].value = 1
+params_mix['amp_R_C'].min = 0
+params_mix['amp_R_N'].value = 1
+params_mix['amp_R_N'].min = 0
 
 with Timer() as t:
     res_model = model.fit(y, x=x, params=params_mix)
 
 print("Parameters:")
 res_model.params.pretty_print()
-amp_Ref1 = res_model.params['amp_Ref1'].value
-amp_Ref2 = res_model.params['amp_Ref2'].value
-print("Estimated proportion:", amp_Ref1/(amp_Ref1+amp_Ref2))
+amp_R_C = res_model.params['amp_R_C'].value
+amp_R_N = res_model.params['amp_R_N'].value
+print("Estimated proportion:", amp_R_C/(amp_R_C+amp_R_N))
 print()
 
 
 
-
-
-dely = res_model.eval_uncertainty(sigma=3)
-
-amp_T1 = res_model.params['amp_Ref1'].value
-amp_T2 = res_model.params['amp_Ref2'].value
-
-kde1 = kde_Ref1(x, amp_T1)
-kde2 = kde_Ref2(x, amp_T2)
-
-
 if plot_results:
-    plt.figure()
-    fig, (axP, axM, axR, axI) = plt.subplots(4, 1, sharex=True, sharey=False)
+    dely = res_model.eval_uncertainty(sigma=3)
 
-    axP.stackplot(x, np.vstack((kde1/(kde1+kde2), kde2/(kde1+kde2))), labels=["Reference 1", "Reference 2"])
+    amp_T1 = res_model.params['amp_R_C'].value
+    amp_T2 = res_model.params['amp_R_N'].value
+
+    kde1 = kde_R_C(x, amp_T1)
+    kde2 = kde_R_N(x, amp_T2)
+
+    # plt.figure()
+    fig, (axP, axM, axR, axI) = plt.subplots(4, 1, sharex=True, sharey=False, figsize=(12, 8))
+
+    axP.stackplot(x, np.vstack((kde1/(kde1+kde2), kde2/(kde1+kde2))), labels=["$R_C$", "$R_N$"])
     legend = axP.legend(facecolor='grey')
     #legend.get_frame().set_facecolor('grey')
-    axP.set_title('Proportions of Ref 1 and Ref 2')
+    axP.set_title('Proportions of R_C and R_N')
 
     plt.sca(axM)
     res_model.plot_fit()
@@ -175,13 +183,14 @@ if plot_results:
     res_model.plot_residuals()
 
     #plt.sca(axI)
-    axI.plot(x, kde1, label='Reference 1')
-    axI.plot(x, kde2, label='Reference 2')
+    axI.plot(x, kde1, label='$R_C$')
+    axI.plot(x, kde2, label='$R_N$')
     axI.legend()
+    plt.savefig("fitting_comparison.png")
 
 if verbose:
     print(res_model.fit_report())
-    print('Ref2/Ref1 =', amp_Ref2/amp_Ref1)
+    print('R_C/R_N =', amp_R_C/amp_R_N)
     print('')
     print('\nParameter confidence intervals:')
     print(res_model.ci_report())  # --> res_mix.ci_out # See also res_mix.conf_interval()
@@ -204,9 +213,9 @@ print("="*40)
 
 # TODO: Switch to using Minimizer to avoid issues with lmfit models
 def KDE_model(pars, x, kdes, data=None):
-    kde_Ref1 = np.exp(kdes['Ref1'][KDE_kernel].score_samples(x[:, np.newaxis]))
-    kde_Ref2 = np.exp(kdes['Ref2'][KDE_kernel].score_samples(x[:, np.newaxis]))
-    model = pars['amp_Ref1'].value * kde_Ref1 + pars['amp_Ref2'].value * kde_Ref2
+    kde_R_C = np.exp(kdes['R_C'][KDE_kernel].score_samples(x[:, np.newaxis]))
+    kde_R_N = np.exp(kdes['R_N'][KDE_kernel].score_samples(x[:, np.newaxis]))
+    model = pars['amp_R_C'].value * kde_R_C + pars['amp_R_N'].value * kde_R_N
     if data is None:
         return model
     else:
@@ -214,13 +223,13 @@ def KDE_model(pars, x, kdes, data=None):
 
 
 with Timer() as t:
-    res_min = minimize(KDE_model, params_mix, args=(x, kdes, y), method=method)
+    res_min = lmfit.minimize(KDE_model, params_mix, args=(x, kdes, y), method=method)
 
 print("Parameters:")
 res_min.params.pretty_print()
-amp_Ref1 = res_min.params['amp_Ref1'].value
-amp_Ref2 = res_min.params['amp_Ref2'].value
-print("Estimated proportion:", amp_Ref1/(amp_Ref1+amp_Ref2))
+amp_R_C = res_min.params['amp_R_C'].value
+amp_R_N = res_min.params['amp_R_N'].value
+print("Estimated proportion:", amp_R_C/(amp_R_C+amp_R_N))
 print()
 
 
@@ -238,18 +247,19 @@ with Timer() as t:
 
 print("Parameters:")
 res_mo.params.pretty_print()
-amp_Ref1 = res_mo.params['amp_Ref1'].value
-amp_Ref2 = res_mo.params['amp_Ref2'].value
-print("Estimated proportion:", amp_Ref1/(amp_Ref1+amp_Ref2))
-print(lmfit.fit_report(res_mo.params))
-lmfit.printfuncs.report_fit(res_mo)
+amp_R_C = res_mo.params['amp_R_C'].value
+amp_R_N = res_mo.params['amp_R_N'].value
+print("Estimated proportion:", amp_R_C/(amp_R_C+amp_R_N))
+if verbose:
+    print(lmfit.fit_report(res_mo.params))
+    lmfit.printfuncs.report_fit(res_mo)
 
-ci = lmfit.conf_interval(mini, res_mo)
-lmfit.printfuncs.report_ci(ci)
-print()
+    ci = lmfit.conf_interval(mini, res_mo)
+    lmfit.printfuncs.report_ci(ci)
+    print()
 
 
 if False:
     # Calculating the posterior probability distribution of parameters
-    res = mini.emcee(burn=300, steps=1000, thin=20, params=mi.params)
+    res = mini.emcee(burn=300, steps=1000, thin=20, params=mini.params)
     corner.corner(res.flatchain, labels=res.var_names, truths=list(res.params.valuesdict().values()))
