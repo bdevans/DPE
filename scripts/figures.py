2bde5ce242e2d5fb39c7c130aa750dae298aea45
Ben Evans <b.d.evans@exeter.ac.uk>
2021-01-13 14:18:30 +0000

    Fix whitespace

--- scripts/figures.py
+++ scripts/figures.py
@@ -30,7 +30,7 @@ import dpe
 from dpe.utilities import construct_mixture, format_seconds, load_accuracy  # get_fpr_tpr,
 from dpe.datasets import (load_diabetes_data, load_renal_data, load_coeliac_data)
 from dpe.plots import (plot_roc, plot_distributions, plot_bootstraps,
-                       plot_characterisation, plot_selected_violins, 
+                       plot_characterisation, plot_selected_violins,
                        get_error_bars)
 # from dpe.config import adjust_excess, ci_method, correct_bias
 
@@ -42,10 +42,10 @@ from dpe.plots import (plot_roc, plot_distributions, plot_bootstraps,
 # TODO: Apply bias correction to other methods e.g. 'bca'?
 # TODO: Report or use correct_bias?
 
-FRESH_DATA = True  # False  # CAUTION!
+FRESH_DATA = True  # False  # CAUTION!
 seed = 42
-n_boot = 10 #1000
-n_mix = 10 #100
+n_boot = 10  # 1000
+n_mix = 10  # 100
 sample_size = 1000  # -1
 n_seeds = 1
 selected_mix = 0
@@ -81,7 +81,7 @@ mpl.rc('figure', titlesize=14)  # fontsize of the figure title
 mpl.rc('lines', linewidth=2)
 mpl.rc('figure', dpi=100)
 mpl.rc('savefig', dpi=600)
-mpl.rc('mpl_toolkits', legacy_colorbar=False)  # Supress MatplotlibDeprecationWarning
+mpl.rc('mpl_toolkits', legacy_colorbar=False)  # Supress MatplotlibDeprecationWarning
 
 # Create output directories
 characterisation_dir = os.path.join("results", "characterisation")
@@ -130,17 +130,15 @@ if __name__ == "__main__":
 
     methods = {method: True for method in dpe._ALL_METHODS_}
     if adjust_excess:
-        adjustment_factor = 1/0.92  # adjusted for fact it underestimates by 8%
+        adjustment_factor = 1 / 0.92  # adjusted for fact it underestimates by 8%
     else:
         adjustment_factor = 1.0
 
-
     for data_label, data in [("Diabetes", load_diabetes_data('T1GRS')),
-                            #  ("Renal", load_renal_data()),
+                             # ("Renal", load_renal_data()),
                              ("Coeliac", load_coeliac_data())]:
 
         (scores, bins, means, medians, p_C) = data
-
 
         if output_application[data_label]:
 
@@ -151,11 +149,10 @@ if __name__ == "__main__":
                 t = time.time()  # Start timer
 
                 summary, df_pe = dpe.analyse_mixture(scores, bins, methods,
-                                        n_boot=n_boot, boot_size=-1, n_mix=n_mix, # boot_size=sample_size,
-                                        alpha=alpha, true_pC=p_C, 
-                                        ci_method=ci_method,
+                                        n_boot=n_boot, boot_size=-1, n_mix=n_mix,  # boot_size=sample_size,
+                                        alpha=alpha, ci_method=ci_method,
                                         correct_bias=correct_bias, n_jobs=-1,  # Previously correct_bias defaulted to False
-                                        logfile=os.path.join(out_dir, f"pe_{data_label}.log"))
+                                        true_pC=p_C, logfile=os.path.join(out_dir, f"pe_{data_label}.log"))
 
                 elapsed = time.time() - t
                 print(f'Elapsed time = {elapsed:.3f} seconds\n')
@@ -170,12 +167,11 @@ if __name__ == "__main__":
                     warnings.warn(f"Missing data file: {res_file}")
                     break
 
-
             # Plot worked examples
             print(f"Plotting application with {data_label} scores...", flush=True)
             fig_ex = plt.figure(figsize=(12, 4))
             gs = plt.GridSpec(nrows=1, ncols=2, hspace=0.15, wspace=0.15,
-                            left=0.08, right=0.95, bottom=0.15, top=0.96)
+                              left=0.08, right=0.95, bottom=0.15, top=0.96)
 
             # sns.set_style("ticks")
             with sns.axes_style("ticks"):
@@ -185,9 +181,9 @@ if __name__ == "__main__":
             # with sns.axes_style("whitegrid"):
             with sns.axes_style("ticks", {"axes.grid": True, "axes.spines.left": False, 'ytick.left': False}):
                 ax_ci_ex = fig_ex.add_subplot(gs[0, 1])
-                plot_bootstraps(df_pe, correct_bias=correct_bias, p_C=p_C, 
-                                ax=ax_ci_ex, limits=application_xlims[data_label], 
-                                ci_method=ci_method, initial=False, legend=False, 
+                plot_bootstraps(df_pe, correct_bias=correct_bias, p_C=p_C,
+                                ax=ax_ci_ex, limits=application_xlims[data_label],
+                                ci_method=ci_method, initial=False, legend=False,
                                 violins=True, orient='h', average=average)
                 # if application_xlims[data_label]:
                 #     ax_ci_ex.set_xlim(*application_xlims[data_label])
@@ -195,12 +191,10 @@ if __name__ == "__main__":
             fig_ex.savefig(os.path.join(fig_dir, f'application_{data_label}.png'))
             fig_ex.savefig(os.path.join(fig_dir, f'application_{data_label}.svg'), transparent=True)
 
-
-            
         if output_characterisation[data_label]:
 
-            #if FRESH_DATA:
-            #    exec(open("./bootstrap.py").read())
+            # if FRESH_DATA:
+            #     exec(open("./bootstrap.py").read())
 
             # Load bootstraps of accurarcy data
             (point_estimates, boots_estimates, proportions, sample_sizes) = load_accuracy(characterisation_dir, data_label)
@@ -217,8 +211,6 @@ if __name__ == "__main__":
                 print("Plotting bootstrapped characterisation of {} scores...".format(data_label), flush=True)
                 fig = plot_characterisation(boots_estimates, proportions, sample_sizes, average=average)
                 fig.savefig(os.path.join(fig_dir, 'boots_characterise_{}.png'.format(data_label)))
-
-
 
         if output_analysis[data_label]:
 
@@ -239,16 +231,16 @@ if __name__ == "__main__":
                 print(f"Running mixture analysis with {data_label} scores...", flush=True)
                 t = time.time()  # Start timer
 
-                # Split the references distributions to ensure i.i.d. data for 
+                # Split the references distributions to ensure i.i.d. data for
                 # constructing the mixtures and estimating them.
                 n_R_C, n_R_N = len(scores['R_C']), len(scores['R_N'])
-                partition_R_C, partition_R_N = n_R_C//2, n_R_N//2
+                partition_R_C, partition_R_N = n_R_C // 2, n_R_N // 2
                 inds_R_C = np.random.permutation(n_R_C)
                 inds_R_N = np.random.permutation(n_R_N)
                 hold_out_scores = {'R_C': scores['R_C'][inds_R_C[:partition_R_C]],
-                                'R_N': scores['R_N'][inds_R_N[:partition_R_N]]}
+                                   'R_N': scores['R_N'][inds_R_N[:partition_R_N]]}
                 violin_scores = {'R_C': scores['R_C'][inds_R_C[partition_R_C:]],
-                                'R_N': scores['R_N'][inds_R_N[partition_R_N:]]}
+                                 'R_N': scores['R_N'][inds_R_N[partition_R_N:]]}
 
                 dfs_point = []
                 dfs_boot = []
@@ -320,11 +312,10 @@ if __name__ == "__main__":
                     warnings.warn(f"Missing data file: {boot_estimates_res_file}")
                     break
 
-
             # Plot selected violins
             print("Plotting violins of constructed mixtures with {} scores...".format(data_label), flush=True)
             plot_mixes = [0]
-            for mix in plot_mixes:  #range(n_seeds):
+            for mix in plot_mixes:  # range(n_seeds):
                 fig = plot_selected_violins(scores, bins, df_point, df_boots, methods, p_stars, sizes,
                                     out_dir, data_label, selected_mix=mix,
                                     add_ci=True, alpha=0.05, ci_method=ci_method,
@@ -341,9 +332,9 @@ if __name__ == "__main__":
 
         # Plot selected violins
         if False:
-            #for mix in range(n_seeds):
+            # for mix in range(n_seeds):
             mix = selected_mix
-            fig_vio = plt.figure(figsize=(8, 2*len(sizes)))
+            fig_vio = plt.figure(figsize=(8, 2 * len(sizes)))
             ax_vio = fig_vio.add_subplot(111)
 
             for p, p_star in enumerate(p_stars):
@@ -354,8 +345,7 @@ if __name__ == "__main__":
                     warnings.simplefilter("ignore", category=FutureWarning)
                     g = sns.violinplot(x='Estimate', y='Size', hue='Method', data=df,
                                        ax=ax_vio, orient='h', cut=0)
-                g.axvline(x=p_star, ymin=0, ymax=1, ls='--')  # ,
-                          # label="Ground Truth: {:3.2}".format(p_star))
+                g.axvline(x=p_star, ymin=0, ymax=1, ls='--')  # label="Ground Truth: {:3.2}".format(p_star))
                 handles, labels = g.get_legend_handles_labels()
                 g.legend(handles, labels[:len(methods)], title="Method")
                 # g.legend.set_title("Method")
@@ -364,11 +354,10 @@ if __name__ == "__main__":
             g.invert_yaxis()
             fig_vio.savefig(os.path.join(fig_dir, 'violin_bootstraps_{}_{}.png'.format(mix, data_label)))
 
-
         # Plot violin stack
         if False:
             print("Plotting violin stacks with {} scores...".format(data_label), flush=True)
-            fig_stack = plt.figure(figsize=(16, 2*len(sizes)))
+            fig_stack = plt.figure(figsize=(16, 2 * len(sizes)))
             gs = plt.GridSpec(nrows=1, ncols=len(methods), hspace=0.2, wspace=0.015)
 
             for m, method in enumerate(methods):
@@ -390,15 +379,13 @@ if __name__ == "__main__":
                         warnings.simplefilter("ignore", category=FutureWarning)
                         g = sns.violinplot(x='Estimate', y='Size', hue='Mix', data=df[df['Method'] == method],
                                            ax=ax_stack, orient='h', cut=0)
-                    g.axvline(x=p_star, ymin=0, ymax=1, ls='--')  # ,
-                               #label="Ground Truth: {:3.2}".format(p_star))
+                    g.axvline(x=p_star, ymin=0, ymax=1, ls='--')  # ,label="Ground Truth: {:3.2}".format(p_star))
                     handles, labels = g.get_legend_handles_labels()
                     g.legend(handles, labels[:n_seeds], title="Mixture")
                     # g.legend.set_title("Method")
 
                 g.invert_yaxis()
             fig_stack.savefig(os.path.join(fig_dir, 'violin_stacks_{}.png'.format(data_label)))
-
 
         # Plot error bars
         if False:
@@ -409,7 +396,7 @@ if __name__ == "__main__":
             for s, size in enumerate(sizes):
                 for p, p_star in enumerate(p_stars):
                     df = df_boots[(df_boots.Size == size) & np.isclose(df_boots["p_C"], p_star)]
-                    
+
                     # Add confidence intervals
 
                     # df_means = df.groupby('Method').mean()
@@ -431,16 +418,14 @@ if __name__ == "__main__":
                     # ax.errorbar(x=x, y=y, yerr=errors, fmt='none', c='w', lw=6, capsize=14, capthick=6)
                     if orient == 'v':
                         x = ax_vio.get_xticks()
-                        y = centres  # means
-                        ax_vio.errorbar(x=x, y=y, yerr=errors, fmt='s', markersize=7, c=c, lw=4, capsize=12, capthick=4, label="Confidence Intervals ({:3.1%})".format(1-alpha))
+                        y = centres  # means
+                        ax_vio.errorbar(x=x, y=y, yerr=errors, fmt='s', markersize=7, c=c, lw=4, capsize=12, capthick=4, label="Confidence Intervals ({:3.1%})".format(1 - alpha))
                     elif orient == 'h':
-                        x = centres  # means
+                        x = centres  # means
                         y = ax_vio.get_yticks()
-                        ax_vio.errorbar(x=x, y=y, xerr=errors, fmt='s', markersize=7, c=c, lw=4, capsize=12, capthick=4, label="Confidence Intervals ({:3.1%})".format(1-alpha))
+                        ax_vio.errorbar(x=x, y=y, xerr=errors, fmt='s', markersize=7, c=c, lw=4, capsize=12, capthick=4, label="Confidence Intervals ({:3.1%})".format(1 - alpha))
 
-
-                    g.axvline(x=p_star, ymin=0, ymax=1, ls='--')  # ,
-                              # label="Ground Truth: {:3.2}".format(p_star))
+                    g.axvline(x=p_star, ymin=0, ymax=1, ls='--')  # ,label="Ground Truth: {:3.2}".format(p_star))
                     handles, labels = g.get_legend_handles_labels()
                     g.legend(handles, labels[:len(methods)], title="Method")
 
@@ -448,7 +433,6 @@ if __name__ == "__main__":
             sns.despine(top=True, bottom=False, left=False, right=True, trim=True)
             g.invert_yaxis()
             fig_vio.savefig(os.path.join(fig_dir, 'ci_selection_{}.png'.format(data_label)))
-
 
         # Plot mixture distributions - add reference populations?
         if False:
@@ -468,13 +452,11 @@ if __name__ == "__main__":
 
                     with warnings.catch_warnings():
                         warnings.simplefilter("ignore", category=FutureWarning)
-                        sns.distplot(scores["R_C"], bins=bins['edges'], hist=False, kde=True, kde_kws={"shade": True}) # hist=True, norm_hist=True, kde=False)#,
-                                     # label=r"$R_1$", ax=ax_mixes)
+                        sns.distplot(scores["R_C"], bins=bins['edges'], hist=False, kde=True, kde_kws={"shade": True})  # hist=True, norm_hist=True, kde=False)#, label=r"$R_1$", ax=ax_mixes)
                         for p, p_star in enumerate(p_stars):
                             sns.distplot(df_mixes[p_star], bins=bins['edges'], hist=False,
                                          label=r"$p_1^*={}$".format(p_star), ax=ax_mixes)  # \tilde{{M}}: n={},
-                        sns.distplot(scores["R_N"], bins=bins['edges'], hist=False, kde=True, kde_kws={"shade": True})#,
-                                     # label=r"$R_2$", ax=ax_mixes)
+                        sns.distplot(scores["R_N"], bins=bins['edges'], hist=False, kde=True, kde_kws={"shade": True})  # , label=r"$R_2$", ax=ax_mixes)
 
                     ax_mixes.set_ylabel(r"$n={}$".format(size), rotation='horizontal')
                     ax_mixes.set_xlabel("")
@@ -482,7 +464,7 @@ if __name__ == "__main__":
                     sns.despine(top=True, bottom=False, left=True, right=True, trim=True)
                     ax_mixes.set_yticks([])
                     ax_mixes.set_yticklabels([])
-                    if si == len(p_stars)-1:
+                    if si == len(p_stars) - 1:
                         ax_mixes.legend()  # title=r"$p_1^*$") #, ncol=len(p_stars))
                     else:
                         ax_mixes.get_legend().set_visible(False)
@@ -492,7 +474,6 @@ if __name__ == "__main__":
             # ax_base.set_xlabel("GRS")
             ax_base.set_xlabel("Genetic Risk Score")
             fig_mixes.savefig(os.path.join(fig_dir, 'constructions_{}.png'.format(data_label)))
-
 
         # Plot distributions around the estimated proportion with given sample_size from the characterisation data
         # if p_C is not None:
@@ -506,7 +487,7 @@ if __name__ == "__main__":
             # Explore the effect of n_boots
             n_boots = [0, 1, 10, 100, 1000]
 
-            fig, axes = plt.subplots(len(n_boots), 1, sharex=True, figsize=(9, 2*len(n_boots)))
+            fig, axes = plt.subplots(len(n_boots), 1, sharex=True, figsize=(9, 2 * len(n_boots)))
 
             for b, n_boots in enumerate(n_boots):
                 if n_boots == 0:
@@ -544,12 +525,12 @@ if __name__ == "__main__":
 
                                 print("Bootstraps = {}; Method: {}; p_C = {}; size = {}; Mean = {}; SD = {}"
                                       .format(b, method, p_star, size, np.mean(df_tmp["Estimate"] - p_star), np.std(df_tmp["Estimate"] - p_star)))
-                                #.sample(n=100, replace=False)
+                                # .sample(n=100, replace=False)
 
                 df = pd.concat(frames, ignore_index=True)
 
                 with sns.axes_style("whitegrid"):
-                    g = sns.catplot(x="Bootstraps", y="Error", hue="Method", col="p_C", row="Size", row_order=sizes[::-1], data=df, kind="violin") #, margin_titles=True) #, ax=ax_err)
+                    g = sns.catplot(x="Bootstraps", y="Error", hue="Method", col="p_C", row="Size", row_order=sizes[::-1], data=df, kind="violin")  # , margin_titles=True) #, ax=ax_err)
                     g.set(ylim=(-0.5, 0.5)).despine(left="True")
                     # g.despine(left="True")
 
