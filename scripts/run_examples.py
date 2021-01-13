105385a76b5e530aff954d73a5c9dac56b1315ff
Ben Evans <b.d.evans@exeter.ac.uk>
2021-01-13 13:42:39 +0000

    Update examples

--- scripts/run_examples.py
+++ scripts/run_examples.py
@@ -7,8 +7,8 @@ import os
 import numpy as np
 import pandas as pd
 
-import proportion_estimation as pe
-import data_utilities as du
+import dpe
+import dpe.datasets as ds
 
 
 # Synthetic data ground truth proportion of cases (p_C) in the mixtures
@@ -22,7 +22,8 @@ load_data = True  # Change this to False to generate new datasets
 # Less frequently changed parameters
 seed = 42  # Set this to seed the PRNG for reproducibility of results
 n_jobs = -1  # Change this to set the number of CPUs to use in parallel or -1 to use all
-correction = True  # Flag to apply bootstrap correction to the estimate
+ci_method = "bca"  # Method for calculating confidence intervals: Bias-corrected and accelerated bootstrap interval
+correct_bias = True  # Flag to apply bootstrap correction to the estimate
 boot_size = -1  # Size of each bootstrap (set to -1 to make it the same size as the mixture distribution)
 alpha = 0.05  # Used to set the centile for calculating the confidence intervals from bootstrapped estimates
 bins = 'fd'  # Use Freedman-Diaconis rule for binning data by default
@@ -38,14 +39,14 @@ for p_C in example_proportions:
     if load_data:
         # Load the example dataset
         print(f'Loading dataset: {dataset}...')
-        scores = du.load_dataset(f'{dataset}.csv')
+        scores = ds.load_dataset(f'{dataset}.csv')
     else:
         # Generate new data
         print(f'Generating fresh example dataset...')
-        scores = du.generate_dataset(p_C)
+        scores = ds.generate_dataset(p_C)
 
         # Save data
-        du.save_dataset(scores, f'{dataset}.csv')
+        ds.save_dataset(scores, f'{dataset}.csv')
 
     # Create output directory
     out_dir = os.path.join("results", dataset)
@@ -54,17 +55,14 @@ for p_C in example_proportions:
     t = time.time()  # Start timer
     # NOTE: the ground truth p_C is passed to analyse_mixture but is only used
     # for printing in the log file for easy comparison and may be omitted
-    results = pe.analyse_mixture(scores, bins, methods, n_boot=n_boot,
-                                 boot_size=boot_size, n_mix=n_mix, alpha=alpha,
-                                 true_p1=p_C, correction=correction, 
-                                 n_jobs=n_jobs, seed=seed, verbose=verbose, 
-                                 logfile=f'{dataset}.log')
+    results = dpe.analyse_mixture(scores, bins, methods, n_boot=n_boot,
+                                  boot_size=boot_size, n_mix=n_mix, alpha=alpha, 
+                                  ci_method=ci_method, correct_bias=correct_bias,
+                                  seed=seed, n_jobs=n_jobs, verbose=verbose,
+                                  true_p1=p_C, logfile=f'{dataset}.log')
 
-    if correction:
-        bootstrap_results, corrected_estimates = results
-    else:
-        bootstrap_results = results
-    bootstrap_results.to_csv(os.path.join(out_dir, f"{dataset}_bootstraps.csv"), header=True)
+    summary, bootstraps = results
+    bootstraps.to_csv(os.path.join(out_dir, f"{dataset}_bootstraps.csv"), header=True)
     elapsed = time.time() - t
     print(f'Elapsed time = {elapsed:.3f} seconds')
     print('=' * 80, '\n')
