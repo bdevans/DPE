5fab99ff059b2eb2be2db00d859494e7e86cb306
Ben Evans <b.d.evans@exeter.ac.uk>
2021-01-13 14:23:15 +0000

    Fix imports and update

--- scripts/tuning/parallel_test.py
+++ scripts/tuning/parallel_test.py
@@ -8,28 +8,27 @@ Created on Wed Nov  7 18:14:30 2018
 
 import time
 
-import numpy as np
-import proportion_estimation as pe
-from datasets import (load_diabetes_data, load_renal_data)
+import dpe
+import dpe.datasets as ds
 
 out_dir = 'benchmark'
 seed = 42
 n_mix = 5
 bootstraps = 100
-boot_size = -1  #1000  # -1
+boot_size = -1  # 1000  # -1
 alpha = 0.05
-CI_METHOD = "experimental"  #"jeffreys"
+CI_METHOD = "bca"  # "experimental"  # "jeffreys"
 KDE_kernel = 'gaussian'
 
-# (scores, bins, means, medians, p1_star) = load_renal_data()
-(scores, bins, means, medians, p1_star) = load_diabetes_data("T1GRS")
+# (scores, bins, means, medians, p1_star) = ds.load_renal_data()
+(scores, bins, means, medians, p1_star) = ds.load_diabetes_data("T1GRS")
 methods = None
 
 # Multicore
 t = time.time()  # Start timer
-results = pe.analyse_mixture(scores, bins, methods, n_boot=bootstraps, n_mix=n_mix,
-                             boot_size=boot_size, alpha=alpha, true_p1=p1_star,
-                             n_jobs=-1, seed=seed, verbose=1)
+results = dpe.analyse_mixture(scores, bins, methods, n_boot=bootstraps, n_mix=n_mix,
+                              boot_size=boot_size, alpha=alpha, true_p1=p1_star,
+                              n_jobs=-1, seed=seed, verbose=1)
 
 print(results[1].head())
 elapsed = time.time() - t
@@ -37,9 +36,9 @@ print('Elapsed time = {:.3f} seconds\n'.format(elapsed))
 
 # Single core
 t = time.time()  # Start timer
-results = pe.analyse_mixture(scores, bins, methods, n_boot=bootstraps, n_mix=n_mix,
-                             boot_size=boot_size, alpha=alpha, true_p1=p1_star,
-                             n_jobs=1, seed=seed, verbose=1)
+results = dpe.analyse_mixture(scores, bins, methods, n_boot=bootstraps, n_mix=n_mix,
+                              boot_size=boot_size, alpha=alpha, true_p1=p1_star,
+                              n_jobs=1, seed=seed, verbose=1)
 
 print(results[1].head())
 elapsed = time.time() - t
