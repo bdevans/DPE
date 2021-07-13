37dfb363dac390276afadd7fd313f62462a7f59c
SlowinskiPiotr <58263515+SlowinskiPiotr@users.noreply.github.com>
2021-07-13 22:30:38 +0100

    Update example_script.m

--- matlab/example_script.m
+++ matlab/example_script.m
@@ -21,7 +21,6 @@ CI_centile=0.05; %centile for calculating the confidence intervals from bootstra
 %each function has the same output
 %p_I - vector of point estimates p_I(1) is pC, p_I(2) is pN
 %CI - vector of 95% confidence intervals CI(1,2) for p_I(1), CI(3,4) for p_I(2)
-%B - median bias of the method estimated from the bootstrap, median(bootstraps)-p_I(1)
 %
 %Please note that small differences between Matlab and Python example outputs 
 %are due to different pseudorandom number generator (PRNG) implementations.
@@ -31,25 +30,26 @@ CI_centile=0.05; %centile for calculating the confidence intervals from bootstra
 clc
 disp('running')
 tic
-[p_I_xcs,CI_xcs,B_xcs]=est_prp_excess(ref_C,ref_N,mix,CI_centile);
+[p_I_xcs,CI_xcs]=est_prp_excess(ref_C,ref_N,mix,CI_centile);
 toc
 tic
-[p_I_mn,CI_mn,B_mn]=est_prp_mean(ref_C,ref_N,mix,CI_centile);
+[p_I_mn,CI_mn]=est_prp_mean(ref_C,ref_N,mix,CI_centile);
 toc
 tic
-[p_I_emd,CI_emd,B_emd]=est_prp_emd(ref_C,ref_N,mix,CI_centile);
+[p_I_emd,CI_emd]=est_prp_emd(ref_C,ref_N,mix,CI_centile);
 toc
 tic
-[p_I_kde,CI_kde,B_kde]=est_prp_kde(ref_C,ref_N,mix,CI_centile);
+[p_I_kde,CI_kde]=est_prp_kde(ref_C,ref_N,mix,CI_centile);
 toc
 %%
 disp(' ')
 disp(['True pC := ' num2str(true_pC,3)])
 disp('===================================================================')
-disp(['EXCESS:' 9 'pC (95% CI):= ' num2str(p_I_xcs(1),3) ' (' num2str(CI_xcs(1),3) ', ' num2str(CI_xcs(2),3) ')' 9 'Bias:= '  num2str(B_xcs,3)])
+disp(['EXCESS:' 9 'pC (95% C):= ' num2str(p_I_xcs(1),5) ' (' num2str(CI_xcs(1),5) ', ' num2str(CI_xcs(2),5) ')'])
 disp('-------------------------------------------------------------------')
-disp(['MEANS:' 9 'pC (95% CI):= ' num2str(p_I_mn(1),3) ' (' num2str(CI_mn(1),3) ', ' num2str(CI_mn(2),3) ')' 9 'Bias:= '  num2str(B_mn,3)])
+disp(['MEANS:' 9 'pC (95% C):= ' num2str(p_I_mn(1),5) ' (' num2str(CI_mn(1),5) ', ' num2str(CI_mn(2),5) ')'])
 disp('-------------------------------------------------------------------')
-disp(['EMD:' 9 'pC (95% CI):= ' num2str(p_I_emd(1),3) ' (' num2str(CI_emd(1),3) ', ' num2str(CI_emd(2),3) ')' 9 'Bias:= '  num2str(B_emd,3)])
+disp(['EMD:' 9 'pC (95% C):= ' num2str(p_I_emd(1),5) ' (' num2str(CI_emd(1),5) ', ' num2str(CI_emd(2),5) ')'])
 disp('-------------------------------------------------------------------')
-disp(['KDE:' 9 'pC (95% CI):= ' num2str(p_I_kde(1),3) ' (' num2str(CI_kde(1),3) ', ' num2str(CI_kde(2),3) ')' 9 'Bias:= '  num2str(B_kde,3)])
+disp(['KDE:' 9 'pC (95% C):= ' num2str(p_I_kde(1),5) ' (' num2str(CI_kde(1),5) ', ' num2str(CI_kde(2),5) ')'])
+
